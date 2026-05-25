0cb42f28caec57adbd9c28c436505dd1da6795f5
MAOUCHE mohamed imad eddine <121735575+1Dmar@users.noreply.github.com>
2026-05-25 21:39:49 +0100

    Add files via upload

---
+++ bot/Models/Server (1).js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+module.exports = model(
+  "serverinformations-promc",
+  new Schema({
+    serverId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    serverName: {
+      type: String,
+    },
+    javaIP: {
+      type: String,
+    },
+    javaPort: {
+      type: Number,
+      default: 25565,
+    },
+    bedrockIP: {
+      type: String,
+    },
+    bedrockPort: {
+      type: Number,
+      default: 19132,
+    },
+    serverType: {
+      type: String,
+    },
+    wallpaper: {
+      type: String,
+      default: "https://wallpapercave.com/wp/wp10819450.jpg"
+    },
+  })
+);
